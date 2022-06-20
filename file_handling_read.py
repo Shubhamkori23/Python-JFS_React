918e87bfa0907d762e7121914b68b4e8179a156d
Shubham Kori <shubhamkori23@gmail.com>
2022-06-20 17:43:18 +0530

    Commit

---
+++ file_handling_read.py
@@ -0,0 +1,9 @@
+# Python code to illustrate read() mode
+
+file = open("test.txt", "r")
+print(file.read())
+
+# Python code to illustrate read() mode character wise
+
+file = open("test.txt", "r")
+print(file.read(5))
