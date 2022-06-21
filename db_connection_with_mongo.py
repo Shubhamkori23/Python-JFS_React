9d496b5deba073db02be70c67e947f2f965b1e94
Shubham Kori <shubhamkori23@gmail.com>
2022-06-21 18:05:01 +0530

    Commit

---
+++ db_connection_with_mongo.py
@@ -0,0 +1,18 @@
+import pymongo
+
+# mongodb cluster URl
+connectionString = "mongodb+srv://Shubh:6668@cluster0.tt98tdj.mongodb.net/test"
+
+# create database and inset id
+if __name__ == '__main__':
+    client = pymongo.MongoClient(connectionString)
+    db = client['test-database']
+
+    posts = db.posts
+    post_id = posts.insert_one({"p": 1}).inserted_id
+    print(post_id)
+
+    # For checking database exist or not
+    # dblist = client.list_database_names()
+    # if "test-database" in dblist:
+    #     print("The database exists.")
