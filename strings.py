a0f5a15bcf68636bc366b1f32e62478870ebe6d9
Shubham Kori <shubhamkori23@gmail.com>
2022-06-21 12:41:50 +0530

    Commit

---
+++ strings.py
@@ -0,0 +1,33 @@
+# Python Program for Creation of String
+
+# String with single quotes
+print('Hello World')
+
+# String with double quotes
+print("I'm an Alien")
+
+# String with triple quotes
+print('''I'm an Alien and I live in a world of "Alien"''')
+
+# Python Program to Access characters of String
+
+String1 = "AlienWorld"
+
+# Printing First character
+print(String1[0])
+
+# Printing Last character
+print(String1[-1])
+
+# Python Program to Update / delete character of a String
+
+# String1 = "Hello, I'm an Alien"
+
+# Updating a character
+# String1[2] = 'p'
+
+# Deleting a character
+# del String1[2]
+
+# Note : In Python, Update or deletion of characters from a String is not allowed because Strings are immutable.
+# Only new strings can be reassigned to the same name.
